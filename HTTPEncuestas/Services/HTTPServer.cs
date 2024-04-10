b9e76a78027100626892a4107126d8a8f96a196b
2Bralbam <bralbam1@gmail.com>
2024-04-10 09:20:47 -0600

    Se agregaron las rutas en el servidor

--- HTTPEncuestas/Services/HTTPServer.cs
+++ HTTPEncuestas/Services/HTTPServer.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Windows;
 
 namespace HTTPEncuestas.Services
 {
@@ -15,7 +17,7 @@ namespace HTTPEncuestas.Services
         public string TituloEncuesta { get; set; } = null!;
         public HTTPServer()
         {
-            Server.Prefixes.Add("http://*:80/");   
+            Server.Prefixes.Add("http://*:22024/");   
             VMMsg.SetServerStatus += (s, e) =>
             {
                 if (e)
@@ -30,27 +32,78 @@ namespace HTTPEncuestas.Services
         }
         public bool StartServer()
         {
-            //if (!Server.IsListening)
-            //{
-            //    Server.Start();
-            //    new Thread(Escuchar)
-            //    { IsBackground = true }.Start();
-            //    return true;
-            //}
-            //else
-            //{
-            //    return false;
-            //}
+            if (!Server.IsListening)
+            {
+                Server.Start();
+                new Thread(Escuchar)
+                { IsBackground = true }.Start();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             return true;
         }
 
         void Escuchar()
         {
-            while (Server.IsListening)
+            try
             {
-                var context = Server.GetContext();
-                string Pagina = File.ReadAllText("Assets/Pagina.html");
-                var bufferPagina = Encoding.UTF8.GetBytes(Pagina);
+                while (Server.IsListening)
+                {
+                    var context = Server.GetContext();
+                    if(context.Request.Url != null)
+                    {
+                        if(context.Request.Url.LocalPath == "")
+                        {
+                            string Pagina = File.ReadAllText("Assets/Index.html");
+                            var bufferPagina = Encoding.UTF8.GetBytes(Pagina);
+                            context.Response.ContentLength64 = bufferPagina.Length;
+                            context.Response.OutputStream.Write(bufferPagina, 0, bufferPagina.Length);
+                            context.Response.StatusCode = 200;
+                            context.Response.Close();
+                        }
+                        else if(context.Request.Url.LocalPath == "/encuesta")
+                        {
+                            string Pagina = File.ReadAllText("Assets/Encuesta.html");
+                            var bufferPagina = Encoding.UTF8.GetBytes(Pagina);
+                            context.Response.ContentLength64 = bufferPagina.Length;
+                            context.Response.OutputStream.Write(bufferPagina, 0, bufferPagina.Length);
+                            context.Response.StatusCode = 200;
+                            context.Response.Close();
+                        }
+                        else if(context.Request.Url.LocalPath == "/encuesta/respuesta" && context.Request.HttpMethod == "POST")
+                        {
+                            byte[] buffer = new byte[context.Request.ContentLength64];
+                            context.Request.InputStream.Read(buffer, 0, buffer.Length);
+                            string datos = Encoding.UTF8.GetString(buffer);
+                            context.Response.StatusCode = 200;
+                            var diccionario = HttpUtility.ParseQueryString(datos);
+                            string Pagina = File.ReadAllText("Assets/PaginaAgradecimiento.html");
+                            var bufferPagina = Encoding.UTF8.GetBytes(Pagina);
+                            context.Response.ContentLength64 = bufferPagina.Length;
+                            context.Response.OutputStream.Write(bufferPagina, 0, bufferPagina.Length);
+                            context.Response.StatusCode = 200;
+                            context.Response.Close();
+                        }
+                        else
+                        {
+                            context.Response.StatusCode = 404;
+                            context.Response.Close();
+                        }
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 404;
+                        context.Response.Close();
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error en el servidor");
             }
         }
     
